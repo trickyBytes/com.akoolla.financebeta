2530fd33456d9e50b91d3ed5495961f8f54425fe
Richard Tiffin <richard.tiffin@cse.org.uk>
2013-09-18 23:02:44 +0100

    Added comment to schedule type

--- finance-beta-api/src/main/java/com/akoolla/financebeta/scheduling/ScheduleType.java
+++ finance-beta-api/src/main/java/com/akoolla/financebeta/scheduling/ScheduleType.java
@@ -1,5 +1,10 @@
 package com.akoolla.financebeta.scheduling;
 
+/**
+ * Indicates the increments of a schedule
+ * @author richardt
+ * @since 1.0
+ */
 public enum ScheduleType {
 	ONCE,SECOND,MINUTE,HOUR,DAY,WEEK,MONTH,YEAR
 }
